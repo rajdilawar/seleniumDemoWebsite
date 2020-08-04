6ede19052ac3b31740fd726381ee4d38e3ffd72c
Rajdilawar <rajdilawar@MacBook-Pro.fritz.box>
2020-08-04 23:44:01 +0200

    adding new pages

--- script/mainJavaScript.js
+++ script/mainJavaScript.js
@@ -5,3 +5,23 @@ $(function() {
         $('.flex-nav ul').toggleClass('open');
     });
 });
+function checkBoxOnClick() {
+
+    alert('check if my selenium handle this alert');
+}
+function onClickButtonDisplayText() {
+    var textDisplay = document.getElementById("myText").value;
+    document.getElementById("myTextToPrint").innerHTML = textDisplay;
+}
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+function displaySignupInfo() {
+
+    //alert('check if my selenium handle this alert');
+}
