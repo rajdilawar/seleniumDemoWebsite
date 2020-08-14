445eccd8e65337822b44bc065d35abdc8f66d91b
Rajdilawar <rajdilawar@MacBook-Pro.fritz.box>
2020-08-14 15:44:27 +0200

    adding new page

--- script/mainJavaScript.js
+++ script/mainJavaScript.js
@@ -54,3 +54,14 @@ function displaySignupInfo() {
     showMessage.className = "show";
     setTimeout(function(){ showMessage.className = showMessage.className.replace("show", ""); }, 9000);
 }
+
+//modal
+
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
